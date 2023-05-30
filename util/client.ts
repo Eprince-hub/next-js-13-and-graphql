2526db84a293d1e2b3cc8d49bd337927117420b9
Victor Ejike Nwosu <ejikello123@yahoo.com>
2023-05-30 16:39:53 +0200

    Change client for mutiple endpoints

--- util/client.ts
+++ util/client.ts
@@ -1,15 +1,31 @@
-import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
+import { ApolloClient, HttpLink, InMemoryCache, split } from '@apollo/client';
 import { registerApolloClient } from '@apollo/experimental-nextjs-app-support/rsc';
 
-// Query graphql with apollo client in nextjs server component
 export const { getClient } = registerApolloClient(() => {
+  const githubLink = new HttpLink({
+    uri: 'https://api.github.com/graphql',
+    headers: {
+      authorization: `Bearer ${process.env.NEXT_PUBLIC_GIT_HUB_API}`,
+    },
+  });
+
+  const localLink = new HttpLink({
+    // uri: '/api/graphql',
+    uri: 'http://localhost:3000/api/graphql',
+    // credentials: 'same-origin',
+  });
+
+  const link = split(
+    // Split based on the target URI
+    ({ operationName }) => {
+      return operationName.startsWith('Github');
+    },
+    githubLink,
+    localLink,
+  );
+
   return new ApolloClient({
     cache: new InMemoryCache(),
-    link: new HttpLink({
-      uri: 'https://api.github.com/graphql',
-      headers: {
-        authorization: `Bearer ${process.env.NEXT_PUBLIC_GIT_HUB_API}`,
-      },
-    }),
+    link,
   });
 });
